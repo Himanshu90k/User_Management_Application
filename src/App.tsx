1a4c9da6f19e5861fa3ee6889a58d9c1bcd36f00
Himanshu Rawat <himanshurawat90k@gmail.com>
2024-10-07 12:50:14 +0530

    create single user page

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,35 @@
 import MainLayout from './layouts/MainLayout';
 import UsersPage from './pages/UsersPage';
-import SingleUserPage from './pages/SingleUserPage';
+import SingleUserPage, { userLoader } from './pages/SingleUserPage';
 import CreateUserPage from './pages/CreateUserPage';
 import UpdateUserPage from './pages/UpdateUserPage';
 import ContactPage from './pages/ContactPage';
 import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
 
-const App = () => {
-    
-    const router = createBrowserRouter(
-      createRoutesFromElements(
-        <Route path='/' element= {<MainLayout />} >
-          <Route index element= { <UsersPage /> } />
-          <Route path='/:id' element={ <SingleUserPage /> } />
-          <Route path='/create-user' element={ <CreateUserPage /> } />
-          <Route path='/update-user' element={ <UpdateUserPage /> } />
-          <Route path='/contact' element={ <ContactPage /> } />
-        </Route>
-      )
-    );
+const App: React.FC = () => {
 
-    return <RouterProvider router={router} />
+  // Delete Job
+  const deleteUser = async (id: string): Promise<void> => {
+      await fetch(`https://jobs-listing-api.onrender.com/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+
+    return;
+  };
+    
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element= {<MainLayout />} >
+        <Route index element= { <UsersPage /> } />
+        <Route path='/:id' element={ <SingleUserPage deleteUser={ deleteUser } /> } loader={ userLoader } />
+        <Route path='/create-user' element={ <CreateUserPage /> } />
+        <Route path='/update-user' element={ <UpdateUserPage /> } />
+        <Route path='/contact' element={ <ContactPage /> } />
+      </Route>
+    )
+  );
+
+  return <RouterProvider router={router} />
   
 };
 
--- src/pages/SingleUserPage.tsx
+++ src/pages/SingleUserPage.tsx
@@ -1,8 +1,139 @@
-const SingleUserPage = () => {
+import { useLoaderData, useNavigate, LoaderFunction } from 'react-router-dom';
+
+type SingleUserPageProps = {
+    deleteUser: (id: string) => Promise<void>;
+};
+
+// Define the type for address data
+type Address = {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+};
+
+// Define the type for company data
+type Company = {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+};
+
+// Define the type for user data
+type User = {
+    id: string;
+    name: string;
+    username: string;
+    email: string;
+    address: Address;
+    phone: string;
+    website: string;
+    company: Company;
+};
+
+const SingleUserPage: React.FC<SingleUserPageProps> = ( { deleteUser }) => {
+
+    const user = useLoaderData() as User;
+    const navigate = useNavigate();
+
+    const onDeleteClick = (userId: string) => {
+        const confirm = window.confirm("Are you sure you want to delete this listing?")
+
+        if(!confirm) return;
+
+        deleteUser(userId);
+        navigate('/');
+    };
+
+    const onUpdateClick = () => {
+        navigate('/update-user')
+    };
+
     return (
         <>
+            <div className="user-box">
+                <div className="user-container">
+
+                    {/* Name and Username */}
+                    <h2 className="title-name">{user.name}</h2>
+                    <p className="username">{`#${user.username}`}</p>
+                    <hr className="line" />
+
+                    {/* Email */}
+                    <div className="email-container">
+                        <h2 className="email-label">Email:</h2>
+                        <p className="email">{user.email}</p>
+                    </div>
+
+                    {/* Address */}
+                    <div className="address-container">
+                        <h2>Address</h2>
+                        <div className="address-boxes">
+                            <div className="address">
+                                <h3>street:</h3>
+                                <p>{user.address.street}</p>
+                            </div>
+                            <div className="address">
+                                <h3>suite:</h3>
+                                <p>{user.address.suite}</p>
+                            </div>
+                            <div className="address">
+                                <h3>city:</h3>
+                                <p>{user.address.city}</p>
+                            </div>
+                            <div className="address">
+                                <h3>zipcode:</h3>
+                                <p>{user.address.zipcode}</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Phone no */}
+                    <div className="contacts-container">
+                        <div className="contact">
+                            <h3>Phone no:</h3>
+                            <p>{user.phone}</p>
+                        </div>
+                    </div>
+
+                    {/* Website */}
+                    <div className="contacts-container">
+                        <div className="contact">
+                            <h3>Website:</h3>
+                            <p>{user.website}</p>
+                        </div>
+                    </div>
+
+                    {/* Company */}
+                    <div className="contacts-container">
+                        <div className="contact">
+                            <h3>Company:</h3>
+                            <p>{user.company.name}</p>
+                        </div>
+                    </div>
+
+                    {/* Update - Delete button */}
+                    <div className="buttons-container">
+                        <button onClick={ () => onUpdateClick() }>
+                            Update
+                        </button>
+                        <button onClick={ () => onDeleteClick(user.id) }>
+                            Delete
+                        </button>
+                    </div>
+                </div>
+            </div>
+            
+            {/* end line */}
+            <hr className='end-line' />
         </>
     )
 };
 
-export default SingleUserPage;
+const userLoader: LoaderFunction = async ({ params }) => {
+    const res = await fetch(`https://jsonplaceholder.typicode.com/users/${params.id}`);
+    const data = await res.json();
+    return data;
+}
+
+export {SingleUserPage as default, userLoader };
